8869299f07e2f73f4e9d2354cd185dbd4b8ce1a7
deadonted <timofejmitusin60892@gmail.com>
2025-01-25 22:25:45 +0300

    feat: init api-gateway project

--- apps/api-gateway/src/api-gateway.service.ts
+++ apps/api-gateway/src/api-gateway.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ApiGatewayService {
   getHello(): string {
-    return 'Hello api-gateway';
+    return 'Hello World!';
   }
 }
