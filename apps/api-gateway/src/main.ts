4c49e77dceeada0e23abb6a4fbc028f641ac98a5
deadonted <timofejmitusin60892@gmail.com>
2025-01-28 00:32:18 +0300

    fix: main.ts api-gateway - is web app nestjs but all app is microservices nestjs

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { ApiGatewayModule } from './api-gateway.module';
+import { NestFactory } from '@nestjs/core'
+import { ApiGatewayModule } from './api-gateway.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
-  await app.listen(process.env.port ?? 3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(3000);
 }
 bootstrap();
