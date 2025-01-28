07da1cb155260ca4690f54f3890a5a60bbfd1c98
neojelll <timofejmitusin60892@gmail.com>
2025-01-29 00:18:01 +0300

    feat: add users service

---
+++ apps/api-gateway/src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { UserDto } from '@lib/user/dto'
+import { Inject, Injectable } from '@nestjs/common'
+import { ClientKafka } from '@nestjs/microservices'
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @Inject('USERS_MICROSERVICE') private readonly usersClient: ClientKafka,
+  ) {}
+
+  async getUser(userId: string) {
+    const result = this.usersClient.send(process.env.USER_GET_TOPIC, JSON.stringify(userId));
+    return result;
+  }
+
+  async updateUser(userId: string, userUpdateDto: UserDto) {
+    const payload = {
+      userId,
+      ...userUpdateDto,
+    }
+
+    const result = this.usersClient.send(process.env.USER_UPDATE_TOPIC, JSON.stringify(payload));
+    return result;
+  }
+
+  async deleteUser(userId: string) {
+    const result = this.usersClient.send(process.env.USER_DELETE_TOPIC, JSON.stringify(userId));
+    return result;
+  }
+}
