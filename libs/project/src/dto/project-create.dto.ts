8522fc1cbf650f91a1075ff02aade57f099da6c3
neojelll <timofejmitusin60892@gmail.com>
2025-01-30 14:26:36 +0300

    refactor: add ApiProperty decorators in ProjectCreateDto

--- libs/project/src/dto/project-create.dto.ts
+++ libs/project/src/dto/project-create.dto.ts
@@ -1,12 +1,26 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator'
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator'
 
 export class ProjectCreateDto {
-	@IsString()
-	@IsNotEmpty()
-	@MaxLength(30)
+	@IsString({ message: 'Name must be a string' })
+	@IsNotEmpty({ message: 'Name is required'})
+	@MinLength(1, { message: 'Name must be at least 1 character long' })
+	@MaxLength(30, { message: 'Name cannot exceed 30 characters' })
+	@ApiProperty({
+		type: String,
+		minLength: 1,
+    maxLength: 30,
+		description: 'The name of the project, must be between 1 and 30 characters long and should be unique.',
+	})
 	name: string;
 
-	@IsString()
-	@MaxLength(500)
+	@IsString({ message: 'About must be a string' })
+	@MaxLength(500, { message: 'About cannot exceed 500 characters' })
+	@ApiPropertyOptional({
+		type: String,
+		maxLength: 500,
+		description: 'A brief description of the project, maximum 500 characters.',
+		default: '',
+	})
 	about?: string;
 }
