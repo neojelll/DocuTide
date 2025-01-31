b309337a0b9b3d87632a9baaadb61af9cdf8c104
neojelll <timofejmitusin60892@gmail.com>
2025-01-31 15:15:46 +0300

    fix: auth service

--- apps/api-gateway/src/auth/auth.service.ts
+++ apps/api-gateway/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { UserSignInDto, UserSignUpDto } from '@lib/user/dto'
 import { Inject, Injectable } from '@nestjs/common'
 import { ClientKafka } from '@nestjs/microservices'
+import { Observable } from 'rxjs'
 
 @Injectable()
 export class AuthService {
@@ -9,12 +10,12 @@ export class AuthService {
 	) {}
 
 	async signUp(userSignUpDto: UserSignUpDto) {
-		const result = this.authClient.send(process.env.USER_CREATE_TOPIC, JSON.stringify(userSignUpDto));
+		const result: Observable<string> = this.authClient.send(process.env.USER_CREATE_TOPIC, JSON.stringify(userSignUpDto));
 		return result;
 	}
 
 	async signIn(userSignInDto: UserSignInDto) {
-		const result = this.authClient.send(process.env.USER_CREATED_TOPIC, JSON.stringify(userSignInDto));
+		const result: Observable<string> = this.authClient.send(process.env.USER_CREATED_TOPIC, JSON.stringify(userSignInDto));
 		return result;
 	}
 }
