8a5bc14f9505940b51bc51d6557dd4ced1a4fbf7
neojelll <timofejmitusin60892@gmail.com>
2025-02-02 21:18:50 +0300

    feat: add JwtModule settings

--- apps/api-gateway/src/auth/auth.module.ts
+++ apps/api-gateway/src/auth/auth.module.ts
@@ -1,27 +1,35 @@
-import { Module } from '@nestjs/common'
-import { ClientsModule, Transport } from '@nestjs/microservices'
-import { AuthController } from './auth.controller'
-import { AuthService } from './auth.service'
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-	imports: [
-		ClientsModule.register([
-			{
-				name: 'AUTH_MICROSERVICE',
-				transport: Transport.KAFKA,
-				options: {
-					client: {
-						clientId: 'auth',
-						brokers: [process.env.MESSAGE_BROKER_URL || 'localhost:9094'],
-					},
-					consumer: {
-						groupId: 'auth-consumer',
-					},
-				},
-			},
-		]),
-	],
-	providers: [AuthService],
-	controllers: [AuthController],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+    ClientsModule.register([
+      {
+        name: 'AUTH_MICROSERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'auth',
+            brokers: [process.env.MESSAGE_BROKER_URL || 'localhost:9094'],
+          },
+          consumer: {
+            groupId: 'auth-consumer',
+          },
+        },
+      },
+    ]),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
