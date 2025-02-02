1d67dfdf76443b6baf124f673c99f99f9d2efb2b
neojelll <timofejmitusin60892@gmail.com>
2025-02-02 21:21:14 +0300

    feat: protected endpoints

--- apps/api-gateway/src/users/users.controller.ts
+++ apps/api-gateway/src/users/users.controller.ts
@@ -1,7 +1,19 @@
-import { UserUpdateDto } from '@lib/user/dto'
-import { Body, Controller, Delete, Get, Inject, OnModuleInit, Param, Patch, ValidationPipe } from '@nestjs/common'
-import { ClientKafka } from '@nestjs/microservices'
-import { UsersService } from './users.service'
+import { UserUpdateDto } from '@lib/user/dto';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  OnModuleInit,
+  Param,
+  Patch,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ClientKafka } from '@nestjs/microservices';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController implements OnModuleInit {
@@ -10,25 +22,37 @@ export class UsersController implements OnModuleInit {
     @Inject('USERS_MICROSERVICE') private readonly usersClient: ClientKafka,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get(':userId')
   async getUser(@Param('userId') userId: string) {
     return await this.usersService.getUser(userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':userId')
-  async updateUser(@Param('userId') userId: string, @Body(ValidationPipe) userUpdateDto: UserUpdateDto) {
+  async updateUser(
+    @Param('userId') userId: string,
+    @Body(ValidationPipe) userUpdateDto: UserUpdateDto,
+  ) {
     return await this.usersService.updateUser(userId, userUpdateDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':userId')
   async deleteUser(@Param('userId') userId: string) {
     return await this.usersService.deleteUser(userId);
   }
 
   async onModuleInit() {
-    this.usersClient.subscribeToResponseOf(process.env.USER_GET_TOPIC || 'user.get');
-    this.usersClient.subscribeToResponseOf(process.env.USER_UPDATE_TOPIC || 'user.update');
-    this.usersClient.subscribeToResponseOf(process.env.USER_DELETE_TOPIC || 'user.delete');
+    this.usersClient.subscribeToResponseOf(
+      process.env.USER_GET_TOPIC || 'user.get',
+    );
+    this.usersClient.subscribeToResponseOf(
+      process.env.USER_UPDATE_TOPIC || 'user.update',
+    );
+    this.usersClient.subscribeToResponseOf(
+      process.env.USER_DELETE_TOPIC || 'user.delete',
+    );
     await this.usersClient.connect();
   }
 }
