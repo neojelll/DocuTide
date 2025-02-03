60ac55b92d6b70d6fbcd0d49b5823e80d02801ba
neojelll <timofejmitusin60892@gmail.com>
2025-02-03 16:39:13 +0300

    fix: get message from kafka methods

--- apps/api-gateway/src/users/users.service.ts
+++ apps/api-gateway/src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { UserReadDto, UserUpdateDto } from '@lib/user/dto';
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
-import { Observable } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -9,35 +9,41 @@ export class UsersService {
     @Inject('USERS_MICROSERVICE') private readonly usersClient: ClientKafka,
   ) {}
 
-  async getUser(userId: string): Promise<Observable<UserReadDto>> {
-    const result: Observable<UserReadDto> = this.usersClient.send(
-      process.env.USER_GET_TOPIC,
-      JSON.stringify(userId),
+  async getUser(userId: string): Promise<UserReadDto> {
+    const result: UserReadDto = await firstValueFrom(
+      this.usersClient.send(process.env.USER_GET_TOPIC, JSON.stringify(userId)),
     );
+
     return result;
   }
 
   async updateUser(
     userId: string,
     userUpdateDto: UserUpdateDto,
-  ): Promise<Observable<string>> {
+  ): Promise<string> {
     const payload: UserUpdateDto = {
       userId,
       ...userUpdateDto,
     };
 
-    const result: Observable<string> = this.usersClient.send(
-      process.env.USER_UPDATE_TOPIC,
-      JSON.stringify(payload),
+    const result: string = await firstValueFrom(
+      this.usersClient.send(
+        process.env.USER_UPDATE_TOPIC,
+        JSON.stringify(payload),
+      ),
     );
+
     return result;
   }
 
-  async deleteUser(userId: string): Promise<Observable<string>> {
-    const result: Observable<string> = this.usersClient.send(
-      process.env.USER_DELETE_TOPIC,
-      JSON.stringify(userId),
+  async deleteUser(userId: string): Promise<string> {
+    const result: string = await firstValueFrom(
+      this.usersClient.send(
+        process.env.USER_DELETE_TOPIC,
+        JSON.stringify(userId),
+      ),
     );
+
     return result;
   }
 }
