d7fc55906b6d57598d35b11afec29a4d8f8b13fe
neojelll <timofejmitusin60892@gmail.com>
2025-02-05 20:43:45 +0300

    feat: add getDocs and saveDocs methods in docs-editor service

--- apps/api-gateway/src/docs-editor/docs-editor.service.ts
+++ apps/api-gateway/src/docs-editor/docs-editor.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class DocsEditorService {
@@ -7,4 +8,37 @@ export class DocsEditorService {
     @Inject('DOCS_EDITOR_MICROSERVICE')
     private readonly docsEditorClient: ClientKafka,
   ) {}
+
+  // add DTO annotation
+  async saveDocs(userId: string, projectId: string, docsDto) {
+    const payload = {
+      userId,
+      projectId,
+      ...docsDto,
+    };
+    const result = await firstValueFrom(
+      this.docsEditorClient.send(
+        process.env.DOCS_SAVE_TOPIC,
+        JSON.stringify(payload),
+      ),
+    );
+
+    return result;
+  }
+
+  async getDocs(userId: string, projectId: string) {
+    const payload = {
+      userId,
+      projectId,
+    };
+
+    const result = await firstValueFrom(
+      this.docsEditorClient.send(
+        process.env.DOCS_GET_TOPIC,
+        JSON.stringify(payload),
+      ),
+    );
+
+    return result;
+  }
 }
