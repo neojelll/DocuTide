2d9a891a3ab07f2c4a95f0b5a57b5381e738ceb8
neojelll <155233969+neojelll@users.noreply.github.com>
2025-02-05 18:23:53 +0000

    feat: add DocsDto (#26)
    * feat: add documentationDto
    
    * chore: rename docsDto library

---
+++ libs/docs/src/dto/docs.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class DocsDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'user documentation',
+    type: String,
+  })
+  content: string;
+}
