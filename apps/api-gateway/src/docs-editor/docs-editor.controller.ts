5011a926dd72abcba89dd8566bcb8ec17790d3d0
neojelll <timofejmitusin60892@gmail.com>
2025-02-05 20:44:08 +0300

    feat: add routes in docs-editor controller

--- apps/api-gateway/src/docs-editor/docs-editor.controller.ts
+++ apps/api-gateway/src/docs-editor/docs-editor.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Inject, OnModuleInit } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  OnModuleInit,
+  Param,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { DocsEditorService } from './docs-editor.service';
 
-@Controller()
+@Controller('users/:userId/projects/:projectId')
 export class DocsEditorController implements OnModuleInit {
   constructor(
     private readonly docsEditorService: DocsEditorService,
@@ -10,7 +21,32 @@ export class DocsEditorController implements OnModuleInit {
     private readonly docsEditorClient: ClientKafka,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
+  @Post('docs')
+  async saveDocs(
+    @Param('userId') userId: string,
+    @Param('projectId') projectId: string,
+    @Body(ValidationPipe) docsDto, // add DTO annotation
+  ) {
+    return await this.docsEditorService.saveDocs(userId, projectId, docsDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('docs')
+  async getDocs(
+    @Param('userId') userId: string,
+    @Param('projectId') projectId: string,
+  ) {
+    return await this.docsEditorService.getDocs(userId, projectId);
+  }
+
   async onModuleInit() {
+    this.docsEditorClient.subscribeToResponseOf(
+      process.env.DOCS_SAVE_TOPIC || 'docs.save',
+    );
+    this.docsEditorClient.subscribeToResponseOf(
+      process.env.DOCS_GET_TOPIC || 'docs.get',
+    );
     await this.docsEditorClient.connect();
   }
 }
