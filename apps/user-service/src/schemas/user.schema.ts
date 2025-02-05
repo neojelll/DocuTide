c0804a52b6a772ae4b8261d98ac2892db3d70f0d
Fl1s <maksima7th@gmail.com>
2025-02-05 23:16:00 +0700

    chore: extra space between imports and decorator

--- apps/user-service/src/schemas/user.schema.ts
+++ apps/user-service/src/schemas/user.schema.ts
@@ -1,6 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 import { v4 as uuidv4 } from 'uuid';
+
 @Schema({ timestamps: true })
 export class User extends Document {
   @Prop({ default: () => uuidv4(), unique: true })
