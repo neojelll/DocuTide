8e02d3254c56f5c4af7e9c0ce01ebb0cf62906ef
Maxwell <73421069+Fl1s@users.noreply.github.com>
2025-02-07 15:17:02 +0700

    feat/project-service (#32)
    * refactor: refactoring-project-dto
    
    * chore: project-service init
    
    * feat: project-controller-layer
    
    * refactor: project-service-layer init & module-layer + controller-layer refactoring & project-schema init
    
    * feat: project-service-service layer & some refactor
    
    * fix: methods naming
    
    * chore: project-schema default values

---
+++ apps/project-service/src/project-service.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { ProjectController } from './project-service.controller';
+import { ProjectService } from './project-service.service';
+import {ClientsModule, Transport} from "@nestjs/microservices";
+import {ConfigModule} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
+import {Project, ProjectSchema} from "./schemas/project.schema";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://admin:secret@localhost:27017'),
+    MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }]),
+    ClientsModule.register([
+      {
+        name: 'PROJECT_MICROSERVICE',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'project-service',
+            brokers: [process.env.MESSAGE_BROKER_URL || 'localhost:9094'],
+          },
+          consumer: {
+            groupId: 'project-service-consumer',
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [ProjectController],
+  providers: [ProjectService],
+})
+export class ProjectServiceModule {}
---
+++ apps/project-service/src/project-service.service.ts
@@ -0,0 +1,70 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ProjectReadDto, ProjectUpdateDto } from '@lib/project/dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Project, ProjectDocument } from './schemas/project.schema';
+
+@Injectable()
+export class ProjectService {
+  constructor(
+    @InjectModel(Project.name) private projectModel: Model<ProjectDocument>,
+  ) {}
+
+  async createProject(ownerId: string): Promise<ProjectReadDto> {
+    const newProject = new this.projectModel({
+      ownerId: ownerId,
+    });
+    return this.toProjectReadDto(await newProject.save());
+  }
+
+  async getAllProjects(): Promise<ProjectReadDto[]> {
+    const projects = await this.projectModel.find().exec();
+    return projects.map(this.toProjectReadDto);
+  }
+
+  async getProjectById(projectId: string) {
+    const project = await this.projectModel.findOne({ id: projectId });
+    if (!project) {
+      throw new NotFoundException(`Project with id ${projectId} not found.`);
+    }
+    return this.toProjectReadDto(project);
+  }
+
+  async updateProject(
+    projectId: string,
+    data: ProjectUpdateDto,
+  ): Promise<ProjectReadDto> {
+    const updatedProject = await this.projectModel.findOneAndUpdate({
+      id: projectId,
+      data: data,
+    });
+    if (!updatedProject) {
+      throw new NotFoundException(`Project with id ${projectId} not found.`);
+    }
+    return this.toProjectReadDto(updatedProject);
+  }
+
+  async deleteProject(projectId: string) {
+    const deletedProject = await this.projectModel.findOneAndDelete({
+      id: projectId,
+    });
+    if (!deletedProject) {
+      throw new NotFoundException(`Project with id ${projectId} not found.`);
+    }
+    return `Project with ID ${projectId} deleted successfully.`;
+  }
+
+  private toProjectReadDto(project: ProjectDocument): ProjectReadDto {
+    const plainProject = project.toObject();
+
+    return {
+      id: plainProject.id,
+      name: plainProject.name,
+      description: plainProject.description,
+      ownerId: plainProject.ownerId,
+      tags: plainProject.tags,
+      createdAt: plainProject.createdAt,
+      updatedAt: plainProject.updatedAt,
+    };
+  }
+}
