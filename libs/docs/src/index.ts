f72b4895e4519dd0b5c7f39336837b731c705e87
neojelll <timofejmitusin60892@gmail.com>
2025-02-07 11:26:45 +0300

    delete all files

--- libs/docs/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './docs.module';
-export * from './docs.service';
--- libs/project/src/dto/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './project-create.dto'
-export * from './project-read.dto'
-export * from './project-update.dto'
