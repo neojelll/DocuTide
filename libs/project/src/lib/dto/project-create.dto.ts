45845028dfe7dbc4f9fea8f1c6b2fd17a96387ff
neojelll <timofejmitusin60892@gmail.com>
2025-02-09 20:19:13 +0300

    refactor: add constructor in all project DTO

--- libs/project/src/lib/dto/project-create.dto.ts
+++ libs/project/src/lib/dto/project-create.dto.ts
@@ -1,39 +1,51 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import {IsArray, IsNotEmpty, IsString, IsUUID, MaxLength, MinLength} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsArray, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
 export class ProjectCreateDto {
-	@IsUUID()
-	@ApiProperty({
-		description: 'Unique identifier of the user associated with the project.',
-		type: String,
-		example: '123e4567-e89b-12d3-a456-426614174000',
-	})
-	ownerId?: string;
+  @IsUUID()
+  @ApiProperty({
+    description: 'Unique identifier of the user associated with the project.',
+    type: String,
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  ownerId?: string;
 
-	@IsString()
-	@IsNotEmpty()
-	@ApiProperty({
-		description: 'The name of the project',
-		type: String,
-		example: 'Insanely AWESOMEE!!!! project',
-	})
-	name: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'The name of the project',
+    type: String,
+    example: 'Insanely AWESOMEE!!!! project',
+  })
+  name: string;
 
-	@IsString()
-	@IsNotEmpty()
-	@ApiProperty({
-		description: 'Description of the project',
-		type: String,
-		example: 'This is a project.',
-	})
-	description: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Description of the project',
+    type: String,
+    example: 'This is a project.',
+  })
+  description: string;
 
-	@IsArray()
-	@IsString({ each: true })
-	@ApiProperty({
-		description: 'Tags associated with the project',
-		type: [String],
-		example: ['opensource', 'tech'],
-	})
-	tags: string[];
+  @IsArray()
+  @IsString({ each: true })
+  @ApiProperty({
+    description: 'Tags associated with the project',
+    type: [String],
+    example: ['opensource', 'tech'],
+  })
+  tags: string[];
+
+  constructor(params: {
+    ownerId: string;
+    name: string;
+    description: string;
+    tags: string[];
+  }) {
+    this.ownerId = params.ownerId;
+    this.name = params.name;
+    this.description = params.description;
+    this.tags = params.tags;
+  }
 }
