07d7291247419a20be3d6ef55cc4e58817687829
neojelll <timofejmitusin60892@gmail.com>
2025-02-12 22:12:47 +0300

    feat: add rate-limit settings

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './auth/auth.module';
 import { DocsEditorModule } from './docs-editor/docs-editor.module';
 import { ProjectsModule } from './projects/projects.module';
@@ -7,6 +8,12 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: Number(process.env.API_TTL),
+        limit: Number(process.env.API_LIMIT),
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
