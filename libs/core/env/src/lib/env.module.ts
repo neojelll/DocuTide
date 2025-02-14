6d6bd77e74fbf6f0f0addf4c3b7c0f661cb38ab1
neojelll <155233969+neojelll@users.noreply.github.com>
2025-02-14 14:14:58 +0000

    feat: add env library (#46)
    * feat: init env library
    
    * feat: add new env library and env-errors

---
+++ libs/core/env/src/lib/env.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
+  controllers: [],
+  providers: [],
+  exports: [],
+})
+export class EnvModule {}
