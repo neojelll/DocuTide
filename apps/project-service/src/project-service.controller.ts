c46283ddb4d7ea67c8916eedbff006cd2f585782
Fl1s <maksima7th@gmail.com>
2025-02-19 04:14:46 +0700

    hotfix: remove-debug-logs

--- apps/project-service/src/project-service.controller.ts
+++ apps/project-service/src/project-service.controller.ts
@@ -1,8 +1,8 @@
-import {Controller, Param} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { ProjectService } from './project-service.service';
 import { ProjectCreateDto, ProjectUpdateDto } from '@docu-tide/core/dtos';
-import {JwtPayload} from "@docu-tide/core/auth";
+import { JwtPayload } from '@docu-tide/core/auth';
 
 @Controller()
 export class ProjectController {
@@ -10,42 +10,34 @@ export class ProjectController {
 
   @MessagePattern(process.env['PROJECT_CREATE_TOPIC'] || 'project.create')
   async handleCreateProject(@Payload() projectData: ProjectCreateDto) {
-    console.log('Received create project request:', projectData);
-    const result = await this.projectService.createProject(projectData);
-    console.log('Project created successfully:', result);
-    return result;
+    return await this.projectService.createProject(projectData);
   }
 
   @MessagePattern(process.env['PROJECT_GET_ALL_TOPIC'] || 'project.get.all')
   async handleGetAllProjects() {
-    console.log('Received request to get all projects');
-    const result = await this.projectService.getAllProjects();
-    console.log('Fetched all projects:', result);
-    return result;
+    return await this.projectService.getAllProjects();
   }
 
   @MessagePattern(process.env['PROJECT_GET_TOPIC'] || 'project.get')
-  async handleGetProjectByProjectName(@Payload() data: { jwtPayload: JwtPayload; projectName: string }) {
-    console.log('Received request to get project by projectName:', data.projectName);
-    const result = await this.projectService.getProjectByProjectname(data.projectName);
-    console.log('Fetched project by projectName:', result);
-    return result;
+  async handleGetProjectByProjectName(
+    @Payload() data: { jwtPayload: JwtPayload; projectName: string },
+  ) {
+    return await this.projectService.getProjectByProjectname(data.projectName);
   }
 
   @MessagePattern(process.env['PROJECT_UPDATE_TOPIC'] || 'project.update')
   async handleUpdateProject(@Payload() payload: ProjectUpdateDto) {
-    console.log('Received update project request:', payload);
     const { ...projectData } = payload;
-    const result = await this.projectService.updateProject(payload.oldProjectName, projectData);
-    console.log('Project updated successfully:', result);
-    return result;
+    return await this.projectService.updateProject(
+      payload.oldProjectName,
+      projectData,
+    );
   }
 
   @MessagePattern(process.env['PROJECT_DELETE_TOPIC'] || 'project.delete')
-  async handleDeleteProject(@Payload() data: { jwtPayload: JwtPayload; projectName: string }) {
-    console.log('Received delete project request for ID:', data.projectName);
-    const result = await this.projectService.deleteProject(data.projectName);
-    console.log('Project deleted successfully:', result);
-    return result;
+  async handleDeleteProject(
+    @Payload() data: { jwtPayload: JwtPayload; projectName: string },
+  ) {
+    return await this.projectService.deleteProject(data.projectName);
   }
 }
--- apps/project-service/src/project-service.service.ts
+++ apps/project-service/src/project-service.service.ts
@@ -19,78 +19,63 @@ export class ProjectService {
       ...projectData,
       userId: projectData.jwtPayload.sub,
     });
-
     try {
-      const savedProject = await newProject.save();
-      console.log('New project created:', savedProject);
-      return new ProjectGetDto(savedProject).stringify();
+      return new ProjectGetDto(await newProject.save()).stringify();
     } catch (error) {
-      console.error('Error creating project:', error);
+      console.error('[Error creating project: ' + error + ']');
       throw error;
     }
   }
 
-
   async getAllProjects() {
-    console.log('Fetching all projects...');
     const projects = await this.projectModel.find().exec();
-    console.log('Projects fetched:', projects);
-
-    const result = projects.map((project) => {
+    return projects.map((project) => {
       return new ProjectGetDto(project);
     });
-
-    console.log('Fetched all projects:', result);
-    return result;
   }
 
-
-
   async getProjectById(projectId: string): Promise<string> {
-    console.log(`Fetching project by ID: ${projectId}`);
-    const project = await this.projectModel.findOne({ id: projectId });
+    const project = await this.projectModel.findOne({ projectId: projectId });
     if (!project) {
-      console.log(`Project with ID ${projectId} not found.`);
       throw new NotFoundException(`Project with id ${projectId} not found.`);
     }
-    console.log('Project found:', project);
     return new ProjectGetDto(project).stringify();
   }
 
   async getProjectByProjectname(projectName: string): Promise<string> {
-    console.log(`Fetching project by projectName: ${projectName}`);
     const project = await this.projectModel.findOne({ projectName });
     if (!project) {
-      console.log(`Project with projectName ${projectName} not found.`);
-      throw new NotFoundException(`Project with projectName ${projectName} not found.`);
+      throw new NotFoundException(
+        `Project with projectName ${projectName} not found.`,
+      );
     }
-    console.log('Project found:', project);
     return new ProjectGetDto(project).stringify();
   }
 
-  async updateProject(oldProjectName: string, data: ProjectUpdateDto): Promise<string> {
-    console.log(`Updating project with projectName: ${oldProjectName} with data:`, data);
+  async updateProject(
+    oldProjectName: string,
+    data: ProjectUpdateDto,
+  ): Promise<string> {
     const updatedProject = await this.projectModel
-      .findOneAndUpdate({ projectName : oldProjectName }, data, { new: true })
+      .findOneAndUpdate({ projectName: oldProjectName }, data, { new: true })
       .exec();
     if (!updatedProject) {
-      console.log(`Project with name ${oldProjectName} not found for update.`);
-      throw new NotFoundException(`Project with name ${oldProjectName} not found.`);
+      throw new NotFoundException(
+        `Project with name ${oldProjectName} not found.`,
+      );
     }
-    console.log('Updated project:', updatedProject);
     return new ProjectGetDto(updatedProject).stringify();
   }
 
   async deleteProject(projectName: string): Promise<string> {
-    console.log(`Deleting project with ID: ${projectName}`);
     const deletedProject = await this.projectModel
       .findOneAndDelete({ projectName })
       .exec();
     if (!deletedProject) {
-      console.log(`Project with name ${projectName} not found for deletion.`);
-      throw new NotFoundException(`Project with name ${projectName} not found.`);
+      throw new NotFoundException(
+        `Project with name ${projectName} not found.`,
+      );
     }
-    console.log(`Project with name ${projectName} deleted successfully.`);
     return `Project with name ${projectName} deleted successfully.`;
   }
 }
