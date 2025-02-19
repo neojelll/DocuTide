1baa62d9777b94e40170a766876a729424655ae5
Fl1s <maksima7th@gmail.com>
2025-02-20 00:21:28 +0700

    refactor: user-service

--- apps/user-service/src/user-service.controller.ts
+++ apps/user-service/src/user-service.controller.ts
@@ -1,6 +1,5 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
-import * as process from 'node:process';
 import { UserService } from './user-service.service';
 import { JwtPayload } from '@docu-tide/core/auth';
 import {
@@ -13,34 +12,33 @@ import {
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @MessagePattern(process.env.USER_CREATE_TOPIC || 'user.create')
-  async handleRegistration(@Payload() userData: UserSignUpDto) {
-    return await this.userService.createUser(userData);
+  @MessagePattern(process.env.USER_CREATE_TOPIC)
+  handleCreate(@Payload() userData: UserSignUpDto) {
+    return this.userService.createUser(userData);
   }
 
-  @MessagePattern(process.env.USER_CREATED_TOPIC || 'user.created')
-  async handleLogin(@Payload() signInDto: UserSignInDto) {
-    return await this.userService.getUserByUsername(signInDto.username);
+  @MessagePattern(process.env.USER_CREATED_TOPIC)
+  handleLogin(@Payload() signInDto: UserSignInDto) {
+    return this.userService.getUserByUsername(signInDto.username);
   }
 
-  @MessagePattern(process.env.USER_GET_ALL_TOPIC || 'user.get.all')
-  async getAll() {
-    return await this.userService.getAllUsers();
+  @MessagePattern(process.env.USER_GET_ALL_TOPIC)
+  handleGetAll() {
+    return this.userService.getAllUsers();
   }
 
-  @MessagePattern(process.env.USER_GET_TOPIC || 'user.get')
-  async handleGetUserByUserId(@Payload() payload: JwtPayload) {
-    return await this.userService.getUserByUserId(payload.sub);
+  @MessagePattern(process.env.USER_GET_TOPIC)
+  handleGet(@Payload() payload: JwtPayload) {
+    return this.userService.getUserByUserId(payload.sub);
   }
 
-  @MessagePattern(process.env.USER_UPDATE_TOPIC || 'user.update')
-  async handleUpdateUser(@Payload() payload: UserUpdateDto) {
-    const { ...userData } = payload;
-    return await this.userService.updateUser(payload.jwtPayload.sub, userData);
+  @MessagePattern(process.env.USER_UPDATE_TOPIC)
+  handleUpdate(@Payload() payload: UserUpdateDto) {
+    return this.userService.updateUser(payload.jwtPayload.sub, payload);
   }
 
-  @MessagePattern(process.env.USER_DELETE_TOPIC || 'user.delete')
-  async handleDeleteUser(@Payload() user: JwtPayload) {
-    return await this.userService.deleteUser(user.sub);
+  @MessagePattern(process.env.USER_DELETE_TOPIC)
+  handleDelete(@Payload() user: JwtPayload) {
+    return this.userService.deleteUser(user.sub);
   }
 }
