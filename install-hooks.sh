9a1573dd9fb6d7640b5cca21d7bee17a794155bd
neojelll <155233969+neojelll@users.noreply.github.com>
2025-02-22 07:50:20 +0300

    feat: add husky (#79)
    * feat: add husky in dev dependencies
    
    * feat: init .husky file and add hooks
    
    * refactor: remove hooks and install-hooks.sh files

--- install-hooks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cp hooks/pre-commit .git/hooks/pre-commit
-cp hooks/post-commit .git/hooks/post-commit
-
-chmod +x .git/hooks/pre-commit
-chmod +x .git/hooks/post-commit
-
-echo "Git hooks have been installed."
