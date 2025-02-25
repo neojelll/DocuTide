718a508d07d2563582d908e2450302514225b3f4
Fl1s <maksima7th@gmail.com>
2025-02-25 22:09:40 +0700

    feat: add prisma-service to schemas-module

--- libs/core/schemas/src/lib/schemas.module.ts
+++ libs/core/schemas/src/lib/schemas.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class SchemasModule {}
