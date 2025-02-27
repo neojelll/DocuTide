1d42f9532606bde49541eafb72a695fb428b26f2
neojelll <timofejmitusin60892@gmail.com>
2025-02-27 17:47:54 +0300

    feat: add main.ts file with create microservice settings

--- apps/server/auth-service/src/main.ts
+++ apps/server/auth-service/src/main.ts
@@ -1,21 +1,25 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          clientId: process.env['AUTH_SERVICE_CLIENT_ID'],
+          brokers: [process.env['KAFKA_URL']],
+        },
+        consumer: {
+          groupId: process.env['AUTH_SERVICE_GROUP_ID'],
+        },
+      },
+    },
   );
+
+  await app.listen();
 }
 
 bootstrap();
