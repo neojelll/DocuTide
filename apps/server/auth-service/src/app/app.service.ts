888e35aec7a3e489869c70889c2fa0f5f3625f80
neojelll <timofejmitusin60892@gmail.com>
2025-02-27 22:32:04 +0300

    feat: add app service with sign up method

--- apps/server/auth-service/src/app/app.service.ts
+++ apps/server/auth-service/src/app/app.service.ts
@@ -1,8 +1,47 @@
+import { AuthLibService } from '@docu-tide/core/auth';
+import { UserGetDto, UserSignUpDto } from '@docu-tide/core/dtos';
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { SignUpError } from '../errors/sign-up.errors';
+import { UserExists } from '../interfaces/user-exists.interface';
+import { MailService } from './mail/mail.service';
+import { UserRepository } from './repository/user.repository';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  constructor(
+    private readonly auth: AuthLibService,
+    private readonly user: UserRepository,
+    private readonly mail: MailService,
+  ) {}
+
+  async signUp(userSignUpDto: UserSignUpDto): Promise<string | UserExists> {
+    try {
+      console.log(`Started signUp user: ${userSignUpDto.username}`);
+
+      const checkUser: UserExists | null = await this.user.checkUser(
+        userSignUpDto,
+      );
+      if (checkUser !== null) {
+        console.log(`Error signUp user: ${checkUser.message}`);
+        return checkUser;
+      }
+
+      const hashPassword: string = await this.auth.hashPassword(
+        userSignUpDto.password,
+      );
+      const newUser: User = await this.user.createUser(
+        userSignUpDto,
+        hashPassword,
+      );
+
+      await this.mail.sendMail(userSignUpDto.email);
+
+      console.log(`User successfully created: ${JSON.stringify(newUser)}`);
+      return await new UserGetDto(newUser).stringify();
+    } catch (error) {
+      console.error(`Error signUp user: ${error.message}`);
+      throw new SignUpError(`SignUp error ${error.message}`);
+    }
   }
 }
