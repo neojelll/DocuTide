46bb0618dbddec45c0903f6178fc14ce7c208020
neojelll <timofejmitusin60892@gmail.com>
2025-02-27 23:00:50 +0300

    refactor: mail service and add mail-logo

--- apps/server/auth-service/src/app/mail/mail.service.ts
+++ apps/server/auth-service/src/app/mail/mail.service.ts
@@ -11,7 +11,7 @@ export class MailService {
     this.transporter = createTransport({
       host: 'smtp.gmail.com',
       port: 587,
-      secure: Boolean(process.env['MAIL_SECURE']),
+      secure: process.env['MAIL_SECURE'] === 'true',
       auth: {
         user: this.mailAddress,
         pass: process.env['MAIL_PASSWORD'],
@@ -21,13 +21,28 @@ export class MailService {
 
   async sendMail(to: string) {
     const subject = 'Hello, confirm your email in DocuTide service!';
-    const text = process.env['MAIL_CONFIRM_URL'];
+    const text = `Please confirm your email by clicking the following link: ${process.env['MAIL_CONFIRM_URL']}`;
+    const html = `
+      <div style="text-align: center;">
+        <img src="cid:logo" alt="Logo" style="width: 50%; height: auto;" />
+        <p>Please confirm your email by clicking the following link:</p>
+        <a href="${process.env['MAIL_CONFIRM_URL']}">Confirm Email</a>
+      </div>
+    `;
 
     const mailOptions = {
       from: this.mailAddress,
       to,
       subject,
       text,
+      html,
+      attachments: [
+        {
+          filename: 'mail-logo.png',
+          path: 'assets/mail-logo.png',
+          cid: 'logo',
+        },
+      ],
     };
 
     await this.transporter.sendMail(mailOptions);
