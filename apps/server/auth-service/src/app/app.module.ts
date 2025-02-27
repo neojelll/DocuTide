423c28619e181b39231c557d4f9ce0f5b22dcb9d
neojelll <timofejmitusin60892@gmail.com>
2025-02-27 20:26:50 +0300

    feat: add auth module

--- apps/server/auth-service/src/app/app.module.ts
+++ apps/server/auth-service/src/app/app.module.ts
@@ -1,10 +1,22 @@
+import { AuthLibModule } from '@docu-tide/core/auth';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from '../prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailService } from './mail/mail.service';
+import { UserRepository } from './repository/user.repository';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthLibModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserRepository, MailService],
 })
 export class AppModule {}
