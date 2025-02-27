276efdf6dea380e8500eac61810d4b1f19d05c57
neojelll <timofejmitusin60892@gmail.com>
2025-02-27 21:36:32 +0300

    refactor: add log in handle sign up

--- apps/server/auth-service/src/app/app.controller.ts
+++ apps/server/auth-service/src/app/app.controller.ts
@@ -12,6 +12,7 @@ export class AppController {
   async handleSignUp(
     @Payload() userSignUpDto: UserSignUpDto,
   ): Promise<string | UserExists> {
+    console.log('Started handleSignUp');
     return await this.appService.signUp(userSignUpDto);
   }
 }
