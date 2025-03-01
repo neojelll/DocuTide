7dd95afb14ed8fc6c3c8df820590803cc6c4af7e
neojelll <timofejmitusin60892@gmail.com>
2025-03-01 14:17:11 +0300

    fix: imports

--- apps/server/auth-service/src/app/app.module.ts
+++ apps/server/auth-service/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { AuthLibModule } from '@docu-tide/core/auth';
+import { SchemasModule } from '@docu-tide/core/schemas';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from '../prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MailService } from './mail/mail.service';
@@ -10,7 +10,7 @@ import { UserRepository } from './repository/user.repository';
 @Module({
   imports: [
     AuthLibModule,
-    PrismaModule,
+    SchemasModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
