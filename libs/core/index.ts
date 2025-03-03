93cf78b01fac6bfd006cd2d821421102ef9442b8
neojelll <155233969+neojelll@users.noreply.github.com>
2025-03-03 17:31:13 +0300

    feat: add library with custom errors (#94)

--- libs/core/index.ts
+++ libs/core/index.ts
@@ -1,3 +1,4 @@
 export * from './dto/index';
+export * from './errors/index';
 export * from './interfaces/index';
 export * from './schemas/index';
--- libs/core/interfaces/index.ts
+++ libs/core/interfaces/index.ts
@@ -1,4 +1,5 @@
 export * from './dto/document-get.interface';
 export * from './dto/project-get.interface';
 export * from './dto/user-get.interface';
+export * from './errors/custom-error.interface';
 export * from './jwt/jwt.interface';
